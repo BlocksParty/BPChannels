c72247dbc70eab3df8f59b5c9869c57449425a20
BlocksParty <post@blocksparty.net>
2012-03-15 22:58:00 +0100

    First Commit

---
+++ src/me/blocksparty/channels/Channels.java
@@ -0,0 +1,201 @@
+package me.blocksparty.channels;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+
+
+public class Channels extends JavaPlugin{
+	public final Logger logger = Logger.getLogger("Minecraft");
+	public final MyPlayerListener pl = new MyPlayerListener();
+	public static Channels plugin;
+	
+	// Creating The HashMap's For The Different Channels
+	public static Map<String, String> regional = new HashMap<String, String>();
+	public static Map<String, String> world = new HashMap<String, String>();
+	public static Map<String, String> trade = new HashMap<String, String>();
+	public static Map<String, String> advice = new HashMap<String, String>();
+	public static Map<String, String> ooc = new HashMap<String, String>();
+	public static Map<String, String> op = new HashMap<String, String>();
+	public static Map<String, String> gm = new HashMap<String, String>();
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + ", Version " + pdfFile.getVersion() + ", Has Been Enabled!");
+		
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this.pl, this);
+	}
+	
+	
+	// Creating The Different Commands To Assign The Players To The Different Channels
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		
+		// The Regional Channel
+		if(commandLabel.equalsIgnoreCase("regional")){ 									// If The Players Types /regional..
+			if(args.length == 0){ 														// If The Length Of The ARGS Is 0 (Which = They Only Typed /regional, Without The Messages They Want To Send After The Command!
+				player.sendMessage("You Have To Type In Your Message!");				// If This Is The Case, Send The Players This Message!
+			}else{																		// ELSE, IF The Players DID type /regional, And DID Type The Messages They Wanted To Send After The Command..
+				regional.put(player.getName(), "regional");								// Put The PlayerName To The HashMap "regional", So That The Next Message The Players Send, Will Be Assigned To Regional!
+																						// Remove The PlayerName From The Other HashMaps, In Case They Decided To Change Channel (Which = Make Sure Players Doesn't Talk In Multiple Channels!
+				world.remove(player.getName());											
+				trade.remove(player.getName());
+				advice.remove(player.getName());
+				ooc.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";													// Gets The Full ARGS The Players Typed After The Command...
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        
+			        List<Entity> nearbyE = player.getNearbyEntities(100, 100, 100);		// Define The Radius The Channel Will Send The Players Message
+			        for (Entity e : nearbyE) {
+			          if (e instanceof Player) {
+			            Player p = (Player) e;
+			            player.sendMessage(ChatColor.AQUA + "[REGIONAL]" + " " + ChatColor.WHITE + player.getName() + ChatColor.AQUA + ": " + ChatColor.WHITE + fullArgs);  // Sends The Message To The Players That Typed It!
+			            p.sendMessage (ChatColor.AQUA + "[REGIONAL]" + " " + ChatColor.WHITE + player.getName() + ChatColor.AQUA + ": " + ChatColor.WHITE + fullArgs);		// Broadcast The Messages Through The Regional Channel!
+			          }
+			        }
+			        
+				}
+			}
+		
+		if(commandLabel.equalsIgnoreCase("world")){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				world.put(player.getName(), "world");
+				regional.remove(player.getName());
+				trade.remove(player.getName());
+				advice.remove(player.getName());
+				ooc.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.BLUE + "[WORLD]" + " " + ChatColor.WHITE + player.getName() + ChatColor.BLUE + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}
+		
+		if(commandLabel.equalsIgnoreCase("trade")){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				trade.put(player.getName(), "trade");
+				world.remove(player.getName());
+				regional.remove(player.getName());
+				advice.remove(player.getName());
+				ooc.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.GREEN + "[TRADE]" + " " + ChatColor.WHITE + player.getName() + ChatColor.GREEN + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}
+		
+		if(commandLabel.equalsIgnoreCase("advice")){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				advice.put(player.getName(), "advice");
+				world.remove(player.getName());
+				trade.remove(player.getName());
+				regional.remove(player.getName());
+				ooc.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[ADVICE]" + " " + ChatColor.WHITE + player.getName() + ChatColor.LIGHT_PURPLE + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}
+		
+		if(commandLabel.equalsIgnoreCase("ooc")){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				ooc.put(player.getName(), "ooc");
+				world.remove(player.getName());
+				trade.remove(player.getName());
+				advice.remove(player.getName());
+				regional.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.YELLOW + "[OOC]" + " " + ChatColor.WHITE + player.getName() + ChatColor.YELLOW + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}
+		
+		if(commandLabel.equalsIgnoreCase("op")){
+			if(player.isOp()){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				op.put(player.getName(), "op");
+				world.remove(player.getName());
+				trade.remove(player.getName());
+				advice.remove(player.getName());
+				regional.remove(player.getName());
+				ooc.remove(player.getName());
+				gm.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.BLACK + "[OP]" + " " + ChatColor.WHITE + player.getName() + ChatColor.BLACK + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}else{ 
+				player.sendMessage("You Do Not Have Permission To Use This Channel!");
+			}
+		}
+		
+		
+		if(commandLabel.equalsIgnoreCase("gm")){
+			if(player.isOp()){
+			if(args.length == 0){
+				player.sendMessage("You Have To Type In Your Message!");
+			}else{
+				gm.put(player.getName(), "ooc");
+				world.remove(player.getName());
+				trade.remove(player.getName());
+				advice.remove(player.getName());
+				regional.remove(player.getName());
+				ooc.remove(player.getName());
+				 String fullArgs = "";
+			        for (String arg : args) {
+			        	fullArgs += arg + " ";
+			        }
+			        Bukkit.broadcastMessage(ChatColor.DARK_RED + "[GAMEMASTER]" + " " + ChatColor.WHITE + player.getName() + ChatColor.DARK_RED + ": " + ChatColor.WHITE + fullArgs);
+				}
+			}else{ 
+				player.sendMessage("You Do Not Have Permission To Use This Channel!");
+			}
+		}
+		return false;
+		}
+	}
---
+++ src/me/blocksparty/channels/MyPlayerListener.java
@@ -0,0 +1,93 @@
+package me.blocksparty.channels;
+
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class MyPlayerListener implements Listener {
+	public static Channels plugin;
+	
+	// Getting The HashMap's For The Different Channels From The Main Class, best to just reference them. This might be your problem, also why is it static :O! Static where? and no, it isnt my problame, belive me, everything that has to do whit the hashmap works! its this line that dont work:
+	Map<String, String> regional = Channels.regional;
+	Map<String, String> world = Channels.world;
+	Map<String, String> trade = Channels.trade;
+	Map<String, String> advice = Channels.advice;
+	Map<String, String> ooc = Channels.ooc;
+	Map<String, String> op = Channels.op;
+	Map<String, String> gm = Channels.gm;
+	
+	// Auto Sets The Players That Joins The Servers To The /regional Channel!
+@EventHandler
+public void onPlayerJoin(PlayerJoinEvent event){
+	Player player = event.getPlayer();
+	
+	if(Channels.regional.containsKey(player.getName())){
+	return;
+	}else{
+		Channels.regional.put(player.getName(), "regional");
+	}
+}
+
+@EventHandler
+public void onPlayerChat(PlayerChatEvent event){
+	Player player = event.getPlayer();
+		if(regional.containsKey(player.getName())){						// Checks If The Players Is Assigned To The "regional" Channel
+			 List<Entity> nearbyE = player.getNearbyEntities(100, 100, 100);
+			    player.sendMessage(ChatColor.AQUA + "[REGIONAL]" + " " + ChatColor.WHITE + player.getName() + ChatColor.AQUA + ": " + ChatColor.WHITE + event.getMessage());
+		        for (Entity e : nearbyE) {
+		          if (e instanceof Player) {
+		        		  Player p = (Player) e;
+		        		  p.sendMessage(ChatColor.AQUA + "[REGIONAL]" + " " + ChatColor.WHITE + player.getName() + ChatColor.AQUA + ": " + ChatColor.WHITE + event.getMessage());
+		          }
+		        }
+		        event.setCancelled(true);
+		}else if(world.containsKey(player.getName())){
+			for(Player p: player.getWorld().getPlayers()){
+				p.sendMessage(ChatColor.BLUE + "[WORLD]" + " " + ChatColor.WHITE + player.getName() + ChatColor.BLUE + ": " + ChatColor.WHITE + event.getMessage());
+			}
+			event.setCancelled(true);
+		}else if(trade.containsKey(player.getName())){
+			Bukkit.broadcastMessage(ChatColor.GREEN + "[TRADE]" + " " + ChatColor.WHITE + player.getName() + ChatColor.GREEN + ": " + ChatColor.WHITE + event.getMessage());
+			event.setCancelled(true);
+		}else if(advice.containsKey(player.getName())){
+			Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[ADVICE]" + " " + ChatColor.WHITE + player.getName() + ChatColor.LIGHT_PURPLE + ": " + ChatColor.WHITE + event.getMessage());
+			event.setCancelled(true);
+		}else if(ooc.containsKey(player.getName())){
+			Bukkit.broadcastMessage(ChatColor.YELLOW + "[OOC]" + " " + ChatColor.WHITE + player.getName() + ChatColor.YELLOW + ": " + ChatColor.WHITE + event.getMessage());
+			event.setCancelled(true);
+		}else if(op.containsKey(player.getName())){
+			for(Player p: plugin.getServer().getOnlinePlayers()){
+				if(p.isOp()){
+					p.sendMessage(ChatColor.BLACK + "[OP]" + " " + ChatColor.WHITE + player.getName() + ChatColor.BLACK + ": " + ChatColor.WHITE + event.getMessage());
+					event.setCancelled(true);
+				}
+				event.setCancelled(true);
+			}
+			event.setCancelled(true);
+		}else if(gm.containsKey(player.getName())){
+			Bukkit.broadcastMessage(ChatColor.DARK_RED + "[GAMEMASTER]" + " " + ChatColor.WHITE + player.getName() + ChatColor.DARK_RED + ": " + ChatColor.WHITE + event.getMessage());
+			event.setCancelled(true);												
+			// If The Players Is NOT Assigned To Any Channel..
+		}else{			
+			// Send The Message To The Players
+			player.sendMessage("----" + ChatColor.LIGHT_PURPLE + "Welcome To BPChannels" + ChatColor.WHITE + "----");
+			player.sendMessage(ChatColor.AQUA + "/regional" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The Regional Channels!");
+			player.sendMessage(ChatColor.AQUA + "/world" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The World Channels!");
+			player.sendMessage(ChatColor.AQUA + "/trade" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The Trade Channels!");
+			player.sendMessage(ChatColor.AQUA + "/advice" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The Advice Channels!");
+			player.sendMessage(ChatColor.AQUA + "/ooc" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The OOC Channels!");
+			player.sendMessage(ChatColor.AQUA + "/gm" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The GM Channels!");
+			player.sendMessage(ChatColor.AQUA + "/rich" + ChatColor.WHITE + " | " + ChatColor.LIGHT_PURPLE + "The Rich Channels! Only The Rich Can Use This!");
+			player.sendMessage("----" + ChatColor.LIGHT_PURPLE + "Welcome To BPChannels" + ChatColor.WHITE + "----");
+			event.setCancelled(true);													// Cancel The Messages, And Make Sure It Don't Show Up In The Chat
+		}
+}
+}
